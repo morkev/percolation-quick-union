24ddc52b57e1190bb93e3e7231bf5cc39ab882db
Kevin Mora <83437383+morkev@users.noreply.github.com>
2021-09-04 14:30:46 -0600

    v1.2

--- Percolation/a01/PercolationStats.java
+++ Percolation/a01/PercolationStats.java
@@ -1,92 +1,106 @@
 package a01;
 
 import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
 /**
- * 1. All sites blocked -> Percolation
- * 2. While we don't have percolation: choose a site uniformly at random among all blocked sites.
- * (Use StdRandom.Uniform). Open this site (Use Percolation.open())
- * 3. # of sites opened/N*N at percolation = estimate of the percolation threshold.
- * 4. Repeat steps 1 through 3, average the results.
-
- * 7. 
+ * Estimates the percolation threshold. Calculates the mean, standard deviation, and low and high confidence intervals.
+ * 
  */
-
 public class PercolationStats {
-    //private int sitesOpened;    //Count the number of sites that are opened until percolation
+    private int n;
     private int size;    //total number of sites
-    private int count;    //counts number of sites opened
-    private double[] results; /*# of sites opened/N*N at percolation. Used to find average of 
-    multiple iterations (aka T).*/
+    private int sitesOpened;    //counts number of sites opened
+    private double[] fracOpenSites;//Stores the fraction of the  # of opened sites/N*N at percolation in an array.
+    private int repeat; //number of times we repeat simulation, T
     
     /**
-     * Perform T independent experiments on an N­by­N grid.
+     * Monte Carol simulation
+     * Perform T independent experiments on an NxN grid.
      * While we don't have percolation, choose a site (i and j) uniformly at random among all blocked sites.
      * 
-     * @param N = Number of sites of 1 side of the grid
-     * @param T = Number of iterations
+     * @param N - Number of sites of 1 side of the grid
+     * @param T - Number of times we repeat the simulation
      */
     public PercolationStats(int N, int T) {
-        //Throw exception if N or T is less than 0
+        
+        /*Throw exception if N or T is less than 0*/
         if (N <= 0 || T <= 0) {
             throw new IllegalArgumentException ("N and T must be greater than 0.");
         }
-        //Number of sites
-        size = N * N;
-        //Create instance of Percolation. We need to use percolates().
-        Percolation perc = new Percolation(N);
-        //Implement for loop that will iterate T times.
-        for(int t = 0; t < T; t++) {
-            //While we don't have percolation, choose a site uniformly at random and open the site
-            while(!perc.percolates()) {
-                //[0,N), +1 so we can get N's integer value
-                int i = StdRandom.uniform(N)+1;
-                int j = StdRandom.uniform(N)+1;
-                //TODO: might have to move this under isOpen
-                perc.open(i, j);
-                //If the site was opened, increase count
-                if (!perc.isOpen(i, j)) {
-                    count++;
+        fracOpenSites = new double[T];
+        this.repeat= T; //Needed for constructor
+        size = N * N; //Number of sites
+        fracOpenSites = getStats(N,T);
+    }
+    
+    private double[] getStats(int n, int t) {
+        double[] repeat = new double[t];
+        
+        for(int k =0; k<t; k++){
+            Percolation perc = new Percolation(n);
+            int sitesOpened = 0;
+            
+            while(!perc.percolates()){
+                int i = StdRandom.uniform(n);
+                int j = StdRandom.uniform(n);
+                if(!perc.isOpen(i, j)){
+                    perc.open(i, j);
+                    sitesOpened++;
                 }
             }
-            /* After we have percolation: (# of sites opened)/(Number of sites) = estimate of the percolation threshold.
-             * Repeat T times. 
-             */
-            results[t] = count/size;
-            }
-        
+            repeat[k]=((double) sitesOpened)/size;
+        }
+        return repeat;
     }
 
+
+/**
+ * Returns the mean value
+ * Mean: ((fraction of open sites) + (fraction of open sites)+...(fraction of open sites T times)) / T times
+ * 
+ * @return
+ */
+    public double mean() {
+        return StdStats.mean(fracOpenSites);
+    }
+    
     /**
-     * Mean: [(fraction of open sites) + (fraction of open sites)+...(fraction of open sites T times)] / T times
+     * Returns the standard deviation value. 
+     *Standard Deviation: ((fraction of open sites #1 - Mean)^2 +(fraction of open sites #2 - Mean)^2+...(fraction of open sites T times - Mean)^2)/ T times - 1
      * 
      * @return
      */
-    public double mean() {
-        return 0;
-    }
-
-    /**
-     * Standard Deviation: [(fraction of open sites #1 - Mean)^2 +(fraction of open sites #2 - Mean)^2+
-     * ...(fraction of open sites T times - Mean)^2] / T times - 1
-     * TODO: Might have to squre root the final outcome to just get the stddev. Function above have stddev^2
-     * @return
-     */
     public double stddev() {
-        return 0;
+        
+        return StdStats.stddev(fracOpenSites);
     }
 
     /**
+     * Return the low endpoint of 95% confidence interval 
      * Low Confidence: [(mean) - ((1.96 * sqrt of stddev)/())]
      * @return
      */
     public double confidenceLow() {
-        return 0;
+        return mean()-(1.96*stddev()/(Math.sqrt((double)repeat)));
     }
     
-
+    /**
+     * Return the high endpoint of 95% confidence interval 
+     * High Confidence: [(mean)  + ((1.96 * sqrt of stddev)/())]
+     * @return
+     */
     public double confidenceHigh() {
-        return 0;
+        return mean()+(1.96*stddev()/(Math.sqrt((double)repeat)));
+    }
+    
+    public static void main(String[] args) {
+        PercolationStats perc = new PercolationStats(200, 100);
+        System.out.println(perc.mean());
+        System.out.println(perc.stddev());
+        System.out.println(perc.confidenceLow());
+        System.out.println(perc.confidenceHigh());
+        
     }
     
 }
