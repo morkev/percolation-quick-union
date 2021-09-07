d337246cfde162d9fedfa537e0ce7207263d2c95
Kevin Mora <83437383+morkev@users.noreply.github.com>
2021-09-07 13:49:54 -0600

    Final Version

--- Percolation/a01/Percolation.java
+++ Percolation/a01/Percolation.java
@@ -2,17 +2,17 @@ package a01;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-/**
- * Estimates the value of the percolation 
- * threshold via Monte Carlo simulation. 
- * 
- * Conditions:
- * N-by-N grid of sites (each square represents a site).
- * White site = open; Black site = closed.
- * Each site is open with probability p (or blocked with probability 1-p).
- * System percolates iff top and bottom are connected by open sites.
- */
-public class Percolation {
+       /**
+ 	* Models a percolation system.
+ 	* N-by-N grid of sites (each square represents a site).
+ 	* White site = open; Black site = closed.
+ 	* Each site is open with probability p (or blocked with probability 1-p).
+ 	* System percolates iff top and bottom are connected by open sites.
+ 	* 
+	* @author Kevin Mora
+	* @author Jordan Bramhall
+	*/
+	public class Percolation {
 	private int n; // stores N
 	private int size; // Number of sites in the grid
 	private int topRow; // Virtual Top
@@ -35,17 +35,18 @@ public class Percolation {
 	 */
 	public Percolation(int N){
 		if(N < 0) {
-			throw new IllegalArgumentException("N must be greater than 0."); //Throw exception if N is <= 0
+			//Throw exception if N is <= 0
+			throw new IllegalArgumentException("N must be greater than 0.");
 		}
 		this.n = N; // Needed for constructor
 		size = N * N; // Create size of grid
-		grid = new boolean[N][N]; //used to indicate if block is open or closed
+		grid = new boolean[N][N]; // Used to indicate if block is open or closed
 		
 		/* WeightedQuickUnionUF initializes an empty data structure for n number of sites. 
 		 * Since we are using virtual top and bottom, we need to add 2 more sites to the grid.*/
 		unionFind = new WeightedQuickUnionUF(size + 2); 
 		
-		/*size (N*N) is the total number of sites. Sites on grid iterate from 0, so last site
+		/* Size (N*N) is the total number of sites. Sites on grid iterate from 0, so last site
 		 * on the grid would be N*N-1. Since we are using a virtual top, we can dynamically assign the
 		 * integer value of size to the virtual top.*/
 		topRow = size;
@@ -53,16 +54,15 @@ public class Percolation {
 		// Adding 1 to size for virtual bottom
 		bottomRow = size + 1;
 		
-		/* Assigning top and bottom rows of the grid to the virtual top and bottom.*/
-		for (int i = 0; i < n; i++) { //iterating only n times, one side of the grid
-			
-			/* Top of the grid is assigned to virtual top 
-			 * (e.g. if N = 5, we assign sites #0 through #4 to virtual top #25)*/
-		    unionFind.union(i, topRow);
-		    
-		    /* Bottom of the grid is assigned to virtual bottom 
-			 * (e.g. if N = 5, 25-(sites #0 through #4)-1 will give us the bottom numbers of the grid.)*/
-		    unionFind.union(size-i-1, bottomRow);
+		// Assigning top and bottom rows of the grid to the virtual top and bottom.
+		for (int i = 0; i < n; i++) { //iterating only n times, one side of the grid	
+		//Top of the grid is assigned to virtual top 
+		// (e.g. if N = 5, we assign sites #0 through #4 to virtual top #25)
+			unionFind.union(i, topRow);
+		    	// Bottom of the grid is assigned to virtual bottom
+			// (e.g. if N = 5, 25 - (sites #0 through #4) - 1 
+			// will give us the bottom numbers of the grid.)
+		    	unionFind.union(size-i-1, bottomRow);
 		}
 	}
 	
@@ -74,18 +74,17 @@ public class Percolation {
 	 * @param j of type integer
 	 */
 	public void open(int i, int j){
-		inRange(i, j);	// Checks if i and j are within the grid
+	    inRange(i, j); // Checks if i and j are within the grid
 	    grid[i][j] = true; // Set the site at i and j to open
 	    
 	    /* 
-	     * Check site to the left. If that site is open, create union between current site
-	     * and site to the left (p and q).
+	     * Check site to the left. If that site is open, create union 
+	     * between current site and site to the left (p and q).
 	     * If i - 1 is less 0 that means that the site is not on this row (west). 
 	     */
 	    if (i - 1 >= 0 && grid[i - 1][j]) {
 	        unionFind.union(location(i, j), location(i - 1, j));
 	    }
-	    
 	    /* 
 	     * Check the site to the right. If site to the right is open, create union. 
 	     * If i + 1 is greater than n that means that the site is not on this row (east)
@@ -93,7 +92,6 @@ public class Percolation {
 	    if (i + 1 < n && grid[i + 1][j]) {
 	        unionFind.union(location(i, j), location(i + 1, j));
 	    }
-	    
 	    /* 
 	     * Check the site downward. If site bellow is open, create union. 
 	     * If j - 1 is less than 0 that means that the site is not on this column (south)
@@ -101,7 +99,6 @@ public class Percolation {
 	    if (j - 1 >= 0 && grid[i][j - 1]) {
 	        unionFind.union(location(i, j), location(i, j - 1));
 	    }
-	    
 	    /* 
 	     * Check the site to the upward. If site is open, create union. 
 	     * If j + 1 is greater than n that means that the site is not on this column (north)
@@ -109,7 +106,7 @@ public class Percolation {
 	    if (j + 1 < n && grid[i][j + 1]) {
 	        unionFind.union(location(i, j), location(i, j + 1));
 	    }
-	    // number of open cells increases by 1 after condition
+	    // number of open cells increases by 1 after condition (1 of 4)
 	    numberOpen++;
 	}
 	
@@ -118,14 +115,13 @@ public class Percolation {
 	 * 
 	 * @param i of type integer
 	 * @param j of type integer
-	 * @return
 	 */
 	public boolean isOpen(int i, int j){
-		outOfBounds(i, j);	// Checks if site is out of bounds.
-		
+		// Checks if site is out of bounds.
+		outOfBounds(i, j);
 		/* 
-		 * If the site is open, return true, 
-		 * if not, return false
+		 * If the site is open, return true; 
+		 * else, return false.
 		 */
 		 if (grid[i][j] == true) {
 			 return true;
@@ -140,14 +136,12 @@ public class Percolation {
 	 * @param j of type integer
 	 */
 	public boolean isFull(int i, int j){
-		// Checks if site is out of bounds.
-		outOfBounds(i, j);
-		
-		// If the site isn't open, it can't be filled.
+	    // Checks if site is out of bounds.
+	    outOfBounds(i, j);
+	    // If the site isn't open, it can't be filled.
 	    if (!isOpen(i, j)) {
 	    	return false;
 	    }
-	    
 	    /* 
 	     * unionFind.connected() will return 
 	     * true if site at i and j is connected 
@@ -164,7 +158,6 @@ public class Percolation {
 	 * If either are not true, percolation doesn't occur.
 	 */
 	public boolean percolates(){
-		
 		return unionFind.connected(topRow, bottomRow);
 	}	
 	
@@ -173,8 +166,8 @@ public class Percolation {
 	 * the site at i and j.
 	 * 
 	 * e.g. if N = 5, we would have an N*N grid with sites assigned 
-     * with integer values starting from 0 to 24 with rows and columns 
-     * of 5 sites. i and j locate where the site is on this grid. 
+     	 * with integer values starting from 0 to 24 with rows and columns 
+     	 * of 5 sites. i and j locate where the site is on this grid. 
 	 * 
 	 * @param i of type integer
 	 * @param j of type integer
@@ -204,8 +197,8 @@ public class Percolation {
 	 * @param j of type integer
 	 */
 	private void outOfBounds(int i, int j) {
-		if (i > n - 1 || i < 0 || j > n - 1 || j < 0) {
-			throw new IndexOutOfBoundsException("Index out of bounds.");
+	    if (i > n - 1 || i < 0 || j > n - 1 || j < 0) {
+		throw new IndexOutOfBoundsException("Index out of bounds.");
 	    }
 	}
 	
